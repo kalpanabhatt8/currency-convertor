c71aeeb11de9d1ccb7f095b3735aac02cee004d2
Kalpana Bhatt <kalpana@infinites.uk>
2024-09-18 14:29:50 +0530

    --console removed

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-// const factPara=document.querySelector('#fact-para')
-// const btn=document.querySelector('#button')
-// const url="https://cat-fact.herokuapp.com/facts"
-
-// // const getFacts= async()=>{
-// //     const url="https://cat-fact.herokuapp.com/facts"
-// //     console.log("Getting facts...");
-// //     const promise = await fetch(url)
-// //     console.log(promise)
-// //     const data=await promise.json()
-// //     factPara.innerText=data[1].text
-// // }
-
-// function getFacts(){
-//  fetch(url).then((response)=>{
-//         return response.json()
-//     })
-//     .then((data)=>{
-//         console.log(data)
-//         factPara.innerText=data[1].text
-//     })
-// }
-
-// btn.addEventListener('click',getFacts);
 
 
 const Base_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies"
@@ -37,7 +13,6 @@ let msg2 = document.querySelector(".defaultValue2")
 
 for (let select of dropdowns) {
     for (let currCode in currencyList) {
-        // console.log(currCode,currencyList[currCode])
         let option = document.createElement('option');
         option.innerText = currCode;
         option.value = currCode;
@@ -47,12 +22,13 @@ for (let select of dropdowns) {
             option.selected = "selected";
         }
         select.appendChild(option)
-        // console.log(option)
     }
     select.addEventListener("change", (evt) => {
         updateFlag(evt.target)
     })
 }
+
+
 
 function incrementValue() {
     let currentValue = parseInt(numberInput.value, 10);
@@ -77,11 +53,11 @@ numberInput.addEventListener('keydown', (event) => {
 });
 
 
+
+
 const updateFlag = (element) => {
-    // console.log(element)
     let currCode = element.value;
     let countryCode = currencyList[currCode]
-    // console.log(countryCode, countryCode)
     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
     let img = element.parentElement.querySelector("img")
     img.src = newSrc
@@ -91,7 +67,6 @@ const updateFlag = (element) => {
 btn.addEventListener("click", async (evt) => {
     let fromCurrLower = fromCurr.value.toLowerCase()
     let toCurrLower = toCurr.value.toLowerCase()
-    // console.log(toCurrLower,fromCurrLower,"thsihi")
     evt.preventDefault()
     let amount = document.querySelector(".amount input")
     let amtValue = amount.value
@@ -99,29 +74,17 @@ btn.addEventListener("click", async (evt) => {
         amtValue = 1
         amount.value = "1"
     }
-    console.log(amtValue)
-    // console.log(fromCurr.value.toLowerCase(),toCurr.value.toLowerCase())
 
     const URL = `${Base_URL}/${fromCurr.value.toLowerCase()}.json`
 
     let response = await fetch(URL)
-    // console.log(response)
     let data = await response.json()
     const rate = data[fromCurrLower][toCurrLower]
-    console.log(rate)
     msgDiv.classList.remove("hide")
     msg.innerText = `1 ${fromCurr.value} equals to`
     msg2.innerText = `${rate} ${toCurr.value}`
     let finalAmount = amtValue * rate
     amountMsg.innerText = `${amtValue} ${fromCurr.value} =${finalAmount} ${toCurr.value}`
-
-
-    // console.log(data)
-    // console.log(data[fromCurrLower][toCurrLower], "this is change")
-    // console.log(data.fromCurrLower,"Will not work"); // Will not work, because it will look for fromCurrLower in the data
-
-    // let rate=data.toCurr.value.toLowerCase()
-    // console.log(rate,"this is rate")
 
 
 })
